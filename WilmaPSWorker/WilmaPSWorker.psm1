3451e788061ee33a3b678ffe335eea1e80df032e
Petri Asikainen <petri.asikainen@sataedu.fi>
2021-05-26 19:48:43 +0300

    initial commit

--- WilmaPSWorker/WilmaPSWorker.psm1
+++ WilmaPSWorker/WilmaPSWorker.psm1
@@ -1,12 +1,10 @@
 # Dot source public/private functions
-Write-Warning "Starting to load"
 $public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
 $private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
 
 
 foreach ($import in @($public + $private)) {
     try {
-        Write-Warning $import.FullName
         Import-module  $import.FullName
     } catch {
         throw "Unable to import-module [$($import.FullName)]"
