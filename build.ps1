b38ec1fd9e0c968c447e68bf1ef7005ec42addee
Petri Asikainen <petri.asikainen@sataedu.fi>
2021-05-26 20:17:48 +0300

    initial commit

--- build.ps1
+++ build.ps1
@@ -22,10 +22,7 @@ $ErrorActionPreference = 'Stop'
 
 # Bootstrap dependencies
 if ($Bootstrap.IsPresent) {
-    #unload older pester
-    if(Get-Module -Name Pester) {
-        Remove-Module -Name Pester
-    }
+
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     if ((Test-Path -Path ./requirements.psd1)) {
